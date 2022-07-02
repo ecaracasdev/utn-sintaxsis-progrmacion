b51379517dbfb4c0e2e6ecd903dbe99bfe224761
elias.caracas <elias.caracas@eggcooperation.com>
2022-07-02 10:53:31 -0300

    corrigiendo el eliminar

--- ejercicio3/tadAgenda.py
+++ ejercicio3/tadAgenda.py
@@ -20,14 +20,9 @@ def eliminarCita(agenda, index):
     return agenda.remove(index)
 
 def eliminarCitasPorObraSocial(agenda, obraSocial):
-    # newAgenda = []
-    # for cita in agenda:
-    #     if cita[1] != obraSocial:
-    #         newAgenda.append(cita)
-    # agenda = newAgenda.copy()
     i=0
     while i < len(agenda):
-        if agenda[i][0] == obraSocial:
+        if agenda[i][1] == obraSocial:
             agenda.remove(agenda[i])
         else:
             i+=1
